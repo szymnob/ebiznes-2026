5a719492819f15e9d4a1b54b76cf7b2176b8ba7f
szymnob <szymon6457@gmail.com>
2026-03-30 11:42:31 +0200

    lab2 3.5 cotroller endpoints and routes

--- lab2/lab2shop/app/controllers/ProductController.scala
+++ lab2/lab2shop/app/controllers/ProductController.scala
@@ -14,4 +14,34 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
     Product(1, "Laptop", 2999.99),
     Product(2, "Myszka", 49.00)
   )
+
+
+  def getAll = Action { Ok(Json.toJson(products)) }
+
+  def getById(id: Int) = Action {
+    products.find(_.id == id).map(p => Ok(Json.toJson(p))).getOrElse(NotFound)
+  }
+
+  def add = Action(parse.json) { request =>
+    val product = request.body.as[Product]
+    products += product
+    Created(Json.toJson(product))
+  }
+
+  def update(id: Int) = Action(parse.json) { request =>
+    val productData = request.body.as[Product]
+    val index = products.indexWhere(_.id == id)
+    if (index != -1) {
+      products.update(index, productData)
+      Ok(Json.toJson(productData))
+    } else NotFound
+  }
+
+  def delete(id: Int) = Action {
+    val index = products.indexWhere(_.id == id)
+    if (index != -1) {
+      products.remove(index)
+      NoContent
+    } else NotFound
+  }
 }
